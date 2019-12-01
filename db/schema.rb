0286fb0ba33727b0385c7ae293d09d14066fafe3
megafabr <megafabr@yandex.ru>
2019-12-01 07:35:37 +0300

    add routes.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_154415) do
+ActiveRecord::Schema.define(version: 2019_11_29_190413) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2019_11_29_154415) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.string "avatar_url"
   end
 
 end
