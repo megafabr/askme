4217c6b2687d6038f009ded8061a807f554125d5
megafabr <megafabr@yandex.ru>
2019-12-04 23:10:51 +0300

    add downcase to username in table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,55 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Edik',
-        username: 'wildfrog',
-        avatar_url: 'https://img3.goodfon.ru/original/4000x3000/a/9b/glaza-zhivotnye-zelenyy-lyagushka.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Misha',
-        username: 'arist'
-      )
-    ]
+    #@users = [
+    #  User.new(
+    #    id: 1,
+    #    name: 'Edik',
+    #    username: 'wildfrog',
+    #    avatar_url: 'https://img3.goodfon.ru/original/4000x3000/a/9b/glaza-zhivotnye-zelenyy-lyagushka.jpg'
+    #  ),
+    #  User.new(
+    #    id: 2,
+    #    name: 'Misha',
+    #    username: 'arist'
+    #  )
+    #]
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    end
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-      name: 'Prima',
-      username: 'inspector',
-      avatar_url: 'https://img3.goodfon.ru/original/4000x3000/a/9b/'  \
-          'glaza-zhivotnye-zelenyy-lyagushka.jpg'
-    )
+    #@user = User.new(
+    #  name: 'Prima',
+    #  username: 'inspector',
+    #  avatar_url: 'https://img3.goodfon.ru/original/4000x3000/a/9b/'  \
+    #      'glaza-zhivotnye-zelenyy-lyagushka.jpg'
+    #)
+    #
+    #@questions = [
+    #  Question.new(text: 'Как дела?', created_at: Date.parse('02.12.2020')),
+    #  Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('02.12.2020'))
+    #]
+    #
+    #@new_question = Question.new
+  end
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('02.12.2020')),
-      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('02.12.2020'))
-    ]
-
-    @new_question = Question.new
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,115 +2,59 @@
 # Эта библиотека понадобится нам для шифрования.
 require 'openssl'
 
-# Модель пользователя.
-#
-# Каждый экземпляр этого класса — загруженная из БД инфа о конкретном юзере.
 class User < ActiveRecord::Base
-  # Параметры работы для модуля шифрования паролей
+
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
-  # Виртуальное поле, которое не сохраняется в базу. Из него перед сохранением
-  # читается пароль, и сохраняется в базу уже зашифрованная версия пароля в
-  # реальные поля password_salt и password_hash.
   attr_accessor :password
-
-  # Эта команда добавляет связь с моделью Question на уровне объектов она же
-  # добавляет метод .questions к данному объекту.
-  #
-  # Вспоминайте уроки про рельционные БД и связи между таблицами.
-  #
-  # Когда мы вызываем метод questions у экземпляра класса User, рельсы
-  # поймут это как просьбу найти в базе все объекты класса Questions со
-  # значением user_id равный user.id.
+  
   has_many :questions
 
-  before_validation :username_downcase
-  #attr_accessor :username
-  def username_downcase
-    @username = @username.downcase
-  end
-
-  # Валидация, которая проверяет, что поля email и username не пустые и не равны
-  # nil. Если не задан email и username, объект не будет сохранен в базу.
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
   validates :password, presence: true, on: :create
-
-  # Проверка максимальной длины юзернейма пользователя (не больше 40 символов)
   validates :username, length: { maximum: 40 }
-
-  # Проверка формата юзернейма пользователя (только латинские буквы, цифры, и знак _)
   validates :username, format: { with:  /\A[a-zA-Z0-9_]+\z/,
                                  message: "латинские буквы, цифры, и знак _" }
 
-  # Проверка формата электронной почты пользователя
   validates_format_of :email, :with => /@/
-
-  # юзернеймы пользователей попадали только в нижнем регистре
-  # нельзя создать двух пользователей, с никами, отличающимися только регистром букв
-
-
-  # Валидация, которая проверяет совпадения значений полей password и
-  # password_confirmation. Понадобится при создании формы регистрации, чтобы
-  # снизить число ошибочно введенных паролей.
   validates_confirmation_of :password
 
-  # Ошибки валидаций можно посмотреть методом errors.
 
-  # Перед сохранением объекта в базу, создаем зашифрованный пароль, который
-  # будет хранится в БД.
+  before_validation :username_downcase
   before_save :encrypt_password
 
-  # Шифруем пароль, если он задан
-  def encrypt_password
-    if password.present?
-      # Создаем т. н. «соль» — рандомная строка усложняющая задачу хакерам по
-      # взлому пароля, даже если у них окажется наша база данных.
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
-      # восстановить исходный пароль. Однако, если правильный пароль у нас есть,
-      # мы легко можем получить такую же строку и сравнить её с той, что в базе.
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
-
-      # Оба поля окажутся записанными в базу при сохранении (save).
-    end
-  end
-
-  # Служебный метод, преобразующий бинарную строку в 16-ричный формат,
-  # для удобства хранения.
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
 
-  # Основной метод для аутентификации юзера (логина). Проверяет email и пароль,
-  # если пользователь с такой комбинацией есть в базе возвращает этого
-  # пользователя. Если нету — возвращает nil.
   def self.authenticate(email, password)
-    # Сперва находим кандидата по email
     user = find_by(email: email)
-
-    # Если пользователь не найдет, возвращаем nil
     return nil unless user.present?
 
-    # Формируем хэш пароля из того, что передали в метод
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
         password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
     )
 
-    # Обратите внимание: сравнивается password_hash, а оригинальный пароль так
-    # никогда и не сохраняется нигде. Если пароли совпали, возвращаем
-    # пользователя.
     return user if user.password_hash == hashed_password
-
-    # Иначе, возвращаем nil
     nil
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  def username_downcase
+    self.username = username.downcase
   end
 end
