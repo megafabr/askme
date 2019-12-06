f7b07cd4e9ebdd5c73307ca2db03ed6142c8db0c
megafabr <megafabr@yandex.ru>
2019-12-06 15:45:36 +0300

    update css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,9 @@
 //= require activestorage
 //= require_tree .
 
-$(function (){
-    $('#ask-button').click(function (){
-        $('#ask-form').slideToggle(300);
-        return false;
-    });
+$(function(){
+  $('#ask-button').click(function(){
+    $('#ask-form').slideToggle(300);
+    return false;
+  });
 });
