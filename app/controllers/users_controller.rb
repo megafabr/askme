0cdcdcdd106fac0c04fa9f32024c522dbc0c56c6
megafabr <megafabr@yandex.ru>
2019-12-12 12:59:13 +0300

    have logined after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to log_in_path, notice: 'Будьте любезны - зологиньтесь'
-      #redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
     else
       render 'new'
     end
